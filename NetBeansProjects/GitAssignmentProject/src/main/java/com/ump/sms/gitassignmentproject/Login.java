302207567970332956ea4583358aab4e985340ae
Bonginkosi <sihlabelabonginkosi@gmail.com>
2026-04-30 15:42:29 +0200

    Added Login.java with login message on feature-login branch

---
+++ NetBeansProjects/GitAssignmentProject/src/main/java/com/ump/sms/gitassignmentproject/Login.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.ump.sms.gitassignmentproject;
+
+/**
+ *
+ * @author sihla
+ */
+public class Login {
+    public static void main(String[] args) {
+        System.out.println("Welcome! User logged in successfully.");
+    }
+    
+}
